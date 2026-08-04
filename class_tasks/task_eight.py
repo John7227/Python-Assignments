b83a1a16bc5c854c45b9f314cb770b8858e871a7
John7227 <lawjoh30@gmail.com>
2026-08-04 16:15:33 +0100

    Tasks

---
+++ class_tasks/task_eight.py
@@ -0,0 +1,8 @@
+for number in range(1, 10):
+	if(number % 4 == 0):
+		print(number + number**2 + number **3 + number **4 + number **5, end=" ")
+
+
+sum = number + number
+
+print(sum)
--- multiplication_table.py
+++ multiplication_table.py
@@ -1,5 +1,4 @@
-for number in range(1, 13):
-	for value in range(1, 13):
+for number in range(1, 13, 1):
+	for value in range(1, 13, 1):
 		print(number, "X", value, "=", value*number, end= "\t")
 	print()
-
